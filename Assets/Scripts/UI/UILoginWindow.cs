bf67937d6727825625a68b086fbeed1c125ea271
Vitah <linw1225@gmail.com>
2017-02-13 11:20:01 +0800

    代码格式化

--- Assets/Scripts/UI/UILoginWindow.cs
+++ Assets/Scripts/UI/UILoginWindow.cs
@@ -2,43 +2,40 @@
 using UnityEngine.Assertions;
 using UnityEngine.UI;
 
-public class UILoginWindow : MonoBehaviour {
+public class UILoginWindow : MonoBehaviour
+{
 
 	// 用于绑定登陆按钮
 	[SerializeField]
 	private Button loginBtn;
 
-	public void Start()
-	{
+	public void Start ()
+    {
 		// 断言，判断按钮对象是否不为空
-		Assert.IsNotNull(this.loginBtn);
+		Assert.IsNotNull (this.loginBtn);
 
 		// 绑定按钮点击响应函数
-		this.loginBtn.onClick.AddListener(this.OnButtonLoginClick);
+		this.loginBtn.onClick.AddListener (this.OnButtonLoginClick);
 	}
-		
-	private void OnButtonLoginClick()
+
+	private void OnButtonLoginClick ()
 	{
 		Debug.Log ("登陆按钮点击");
 
-		PomeloManager.Destroy();
-		PomeloManager.Instance.Connect((code, data) =>
-			{
-				if (code == ErrorCode.OK)
-				{
-					ConnectorEntryRequest request = new ConnectorEntryRequest();
-					request.OnSuccess = this.OnEntry;
-					request.Entry();
-				}
-				else
-				{
-					Debug.Log("connect fail");
-					this.loginBtn.gameObject.SetActive(true);
-				}
-			});
+		PomeloManager.Destroy ();
+		PomeloManager.Instance.Connect ((code, data) => {
+			if (code == ErrorCode.OK) {
+				ConnectorEntryRequest request = new ConnectorEntryRequest ();
+				request.OnSuccess = this.OnEntry;
+				request.Entry ();
+			} else {
+				Debug.Log ("connect fail");
+				this.loginBtn.gameObject.SetActive (true);
+			}
+		});
 	}
 
-	private void OnEntry(RequestData data)
+	private void OnEntry (RequestData data)
 	{
 		Debug.Log ("OnEntry");
 		Debug.Log (data);

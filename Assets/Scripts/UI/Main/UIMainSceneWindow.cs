f79a2e7c39ed791a1585157cc34f899c04182bbf
Vitah <linw1225@gmail.com>
2017-02-20 21:04:59 +0800

    主场景显示玩家基本信息

---
+++ Assets/Scripts/UI/Main/UIMainSceneWindow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+public class UIMainSceneWindow : MonoBehaviour {
+	[SerializeField]
+	private Text roleInfoText;
+
+	// Use this for initialization
+	void Start () {
+		Assert.IsNotNull (this.roleInfoText);
+		this.showRoleInfo ();
+	}
+	
+	private void showRoleInfo(){
+		this.roleInfoText.text = string.Empty;
+		this.roleInfoText.text = this.getRoleInfoStr ();
+	}
+
+	private string getRoleInfoStr(){
+		return "Uid:" + DataPool.Instance.Role.Uid.ToString () +
+		"  Name:" + DataPool.Instance.Role.Name +
+		"  Lv:" + DataPool.Instance.Role.Lv.ToString () +
+		"  Gold:" + DataPool.Instance.Role.Gold.ToString () +
+		"  Diamond:" + DataPool.Instance.Role.Diamond.ToString ();
+	}
+}

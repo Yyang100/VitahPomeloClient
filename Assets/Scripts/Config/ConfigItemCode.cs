0e1b7280d0fe479304e453c5bf71590c5f82b79d
Vitah <linw1225@gmail.com>
2017-05-08 14:47:30 +0800

    增加错误码

---
+++ Assets/Scripts/Config/ConfigItemCode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SimpleJson;
+using UnityEngine;
+using System.Collections;
+
+public class ConfigItemCode
+{
+	public int Id { get; private set; }
+
+	public string Dscp { get; private set; }
+
+	public int Alert { get; private set; }
+
+	public void LoadFromJson (JsonObject jsonObject)
+	{
+		if (jsonObject == null) {
+			Debug.Log ("json文件读取错误");
+		} else {
+			this.Id = JsonUtil.GetInt32 (jsonObject, "Id");
+			this.Dscp = JsonUtil.GetString (jsonObject, "Dscp");
+			this.Alert = JsonUtil.GetInt32 (jsonObject, "Alert");
+		}
+	}
+}
---
+++ Assets/Scripts/Config/ConfigManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using SimpleJson;
+
+public class ConfigManager {
+
+	private static ConfigManager instance = null;
+
+	private ConfigCode configCode = new ConfigCode();
+
+	private ConfigManager() 
+	{
+	}
+
+	public static ConfigManager Instance() 
+	{
+		if (instance == null) 
+		{
+			instance = new ConfigManager();
+			instance.Load();
+		}
+
+		return instance;
+	}
+
+	public ConfigCode Code()
+	{
+		return this.configCode;
+	}
+
+	public void Load() 
+	{
+		this.configCode.LoadFromJson(this.JsonFromFile("code"));
+	}
+
+	public JsonObject JsonFromFile(string filepath) 
+	{
+		JsonObject jsonObject = null;
+		TextAsset fileName = UnityEditor.AssetDatabase.LoadAssetAtPath ("Assets/Config/" + filepath + ".json", typeof(TextAsset)) as TextAsset;
+
+		if (fileName == null)
+		{
+			Debug.Log("find json file faild!");
+		}
+		else
+		{
+			jsonObject = SimpleJson.SimpleJson.DeserializeObject<JsonObject>(fileName.text);
+		}
+
+		return jsonObject;
+	}
+}
